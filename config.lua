d3bc15ec95439181057fb9111350cc3d3d07102b
DonHulieo <96005229+DonHulieo@users.noreply.github.com>
2023-09-17 15:47:20 +1000

    Add files via upload

---
+++ config.lua
@@ -0,0 +1,3 @@
+Config = {}
+
+Config.Key = 51 -- E
---
+++ fxmanifest.lua
@@ -0,0 +1,12 @@
+fx_version 'cerulean'
+game 'gta5'
+
+description 'Don Dockhandler - A FiveM Implementation of the Native Dockhandler'
+
+version '1.0.0'
+
+shared_script 'config.lua'
+
+client_script 'client/main.lua'
+
+lua54 'yes'
